7d3af2d8b420beac5d626620d88c5b1b210539db
kevintone <kevinton@hawaii.edu>
2016-03-02 15:01:49 -1000

    Added Brick Assets

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	
+	public void LoadLevel(string name)
+	{
+		Debug.Log("Level Requested for: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest()
+	{
+		Debug.Log("I want to Quit");
+		Application.Quit();
+	}
+}
